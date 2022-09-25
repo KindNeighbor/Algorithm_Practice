22df86c9500a34a0dfbca9c575ddde3cd2e95cca
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-09-26 03:24:53 +0900

    [level 1] Title: 소수 찾기, Time: 13.54 ms, Memory: 55.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        int[] arr = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 1) {
+                continue;
+            }
+            for (int j = i + i; j <= n; j += i) {
+                    arr[j] = 1;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
