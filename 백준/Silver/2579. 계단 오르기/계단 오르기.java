1f354bd06b576aa22a2af1f990059c6cf5d834b0
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-09-30 22:00:18 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[T+1];
+        for (int i = 1; i <= T; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[T+1];
+
+        dp[0] = arr[0];
+        dp[1] = arr[1];
+        
+        if (T >= 2) {
+            dp[2] = arr[2] + arr[1];
+        }
+
+        for (int i = 3; i <= T; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
+        }
+
+        System.out.println(dp[T]);
+    }
+}
