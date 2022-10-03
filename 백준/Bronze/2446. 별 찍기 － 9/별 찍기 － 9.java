99e25d9a6edcf837c6e3430bb622a815ac00c9f6
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-03 13:01:23 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 476 ms, Memory: 20984 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2*N - 1 - 2*i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i; j < N - 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i + 3; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
