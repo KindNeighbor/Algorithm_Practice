62bff067c6546bf6658406e6c81ebf2bd921a099
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-05 01:42:05 +0900

    [level 2] Title: 올바른 괄호, Time: 17.38 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] c = s.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '(') {
+                stack.push(c[i]);
+            } else if (c[i] == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
