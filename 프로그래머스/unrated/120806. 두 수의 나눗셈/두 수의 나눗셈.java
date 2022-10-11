9ffc52a890ca28387f9c7865ebbf6a5b05f9da0f
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-11 16:27:16 +0900

    [unrated] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double a = ((double)num1 / (double)num2); 
+        double b = a * 1000;
+        int c = (int)b;
+        return c;
+    }
+}
