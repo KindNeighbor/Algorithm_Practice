bd9bd28e8197386d5e84000625bae73745bf6f9e
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-11 16:21:07 +0900

    [unrated] Title: 문자열 정렬하기 (2), Time: 0.59 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,18 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        String s = my_string.toLowerCase();
-        ArrayList<Character> list = new ArrayList<>();
-        char[] c = s.toCharArray();
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < c.length; i++) {
-            list.add(c[i]);
+            sb.append(c[i]);
         }
-        
-        Collections.sort(list);
-        String s1 = "";
-        for (int i = 0; i < list.size(); i++) {
-            s1 += list.get(i);
-        }
-        return s1;
+        return sb.toString();
     }
 }
