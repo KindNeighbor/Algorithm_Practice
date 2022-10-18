28d6a5cc7e9060319d6d6d66dd66e106e13703b2
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-18 17:02:49 +0900

    [Bronze I] Title: 분수찾기, Time: 148 ms, Memory: 16072 KB -BaekjoonHub

--- 백준/Bronze/1193. 분수찾기/분수찾기.java
+++ 백준/Bronze/1193. 분수찾기/분수찾기.java
@@ -1,28 +1,50 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int X = Integer.parseInt(br.readLine());
-        int cross_count = 1; 
-        int count_sum = 0;
-        while(true) {
-            if(X <= cross_count + count_sum) {
-                if(cross_count%2 == 1) {
-                    System.out.println((cross_count-(X-count_sum-1)) + "/" + (X-count_sum));
-                    break;
-                }
-                else {
-                    System.out.println((X-count_sum) + "/" + (cross_count-(X-count_sum-1)));
-                    break;
-                }
-            } else {
-                count_sum = cross_count + count_sum;
-                cross_count++;
+
+        int n = 0;
+        int pre_n = 0;
+        int idx = 1;
+        int cnt = 0;
+        while (n <= X) {
+            pre_n = n;
+            int num = ((idx * (idx - 1)) / 2) + 1;
+            n = num;
+            idx++;
+            cnt++;
+        }
+
+        int a = 0;
+        int b = 0;
+
+        if (cnt % 2 == 0) {
+            a = cnt - 1;
+            b = 1;
+        } else if (cnt % 2 != 0) {
+            a = 1;
+            b = cnt - 1;
+        }
+
+        int length = X - pre_n;
+
+        if (a > b) {
+            for (int i = 0; i < length; i++) {
+                a--;
+                b++;
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                a++;
+                b--;
             }
         }
+
+        System.out.println(a + "/" + b);
     }
 }
