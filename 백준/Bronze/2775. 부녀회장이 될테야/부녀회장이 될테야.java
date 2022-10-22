ad3165d05041def6437920ba6e510d02f42ccdef
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-22 20:37:06 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 124 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,34 +1,34 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.util.Arrays;
 
 public class Main {
-    public static int[][] apt = new int[15][15];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        apt();
-        
         int T = Integer.parseInt(br.readLine());
-        for(int i = 0; i<T; i++) {
+
+        for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            sb.append(apt[k][n]).append('\n');            
-        }
-        System.out.println(sb);
-    }
-    
-    public static void apt() {
-        for(int i=0; i<15; i++) {
-            apt[i][1] = 1;
-            apt[0][i] = i;
-        }
-        
-        for(int i=1; i<15; i++) {
-            for(int j=2; j<15; j++) {
-                apt[i][j] = apt[i][j-1] + apt[i-1][j];
+
+            int[][] arr = new int[k+1][n];
+
+            for (int j = 0; j < n; j++) {
+                arr[0][j] = j+1;
             }
+
+            for (int j = 1; j <= k; j++) {
+                arr[j][0] = 1;
+            }
+
+            for (int j = 1; j <= k; j++) {
+                for (int l = 1; l < n; l++) {
+                    arr[j][l] = arr[j][l-1] + arr[j-1][l];
+                }
+            }
+            
+            System.out.println(arr[k][n-1]);
         }
     }
 }
