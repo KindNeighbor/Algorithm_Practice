eebc045c2cbb394aa2fe3bdbb17ef2e8a185b3e5
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-23 14:32:52 +0900

    [Bronze III] Title: 컵라면 측정하기, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
+++ 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
@@ -11,9 +11,12 @@ public class Main {
 
         int D1 = Integer.parseInt(st.nextToken());
         int D2 = Integer.parseInt(st.nextToken());
-
-        double a = (double) (D1 - D2) / 2;
-        double b = K*K - a*a;
-        System.out.println(b);
+        if (D1 == D2) {
+            System.out.println(K*K);
+        } else {
+            double a = (double) (D1 - D2) / 2;
+            double b = K * K - a * a;
+            System.out.println(b);
+        }
     }
 }
