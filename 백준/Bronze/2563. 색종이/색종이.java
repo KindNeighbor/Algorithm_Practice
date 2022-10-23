1ee17521d57f2ebd60d0ad3635436ad3bf162d49
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-24 02:15:42 +0900

    [Bronze I] Title: 색종이, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] arr = new int[100][100];
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int j = a; j < a+10; j++) {
+                for (int k = b; k < b+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
