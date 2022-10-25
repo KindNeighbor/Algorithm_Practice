9f3840c24bb5c94cce6dcfa24a79ed0068571708
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-25 09:43:43 +0900

    [Bronze I] Title: 소인수분해, Time: 164 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,24 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        
-        for(int i=2; i<N; i++) {
-            while(N%i ==0) {
-                sb.append(i).append('\n');
-                N /= i;
+
+        StringBuilder sb = new StringBuilder();
+        int idx = 2;
+        while (N > 1) {
+            while (true) {
+                if (N % idx == 0) {
+                    N /= idx;
+                    sb.append(idx).append("\n");
+                } else {
+                    break;
+                }
             }
+            idx++;
         }
-        if(N != 1) {
-            sb.append(N);
-        }
+
         System.out.println(sb);
     }
 }
-        
