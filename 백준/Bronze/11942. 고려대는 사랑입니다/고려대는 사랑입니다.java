e2d5f4d54e80860f7658cf641d3963af86e89f7e
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-31 07:44:33 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 116 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("고려대학교");
+        System.out.println(sb);
+
+    }
+}
