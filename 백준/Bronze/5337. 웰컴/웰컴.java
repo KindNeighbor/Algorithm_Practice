fed33cfe4715e4e34dcf4d4721e9feba9bd65ffc
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-10-31 07:45:54 +0900

    [Bronze V] Title: 웰컴, Time: 112 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+
+        System.out.println(sb);
+
+    }
+}
