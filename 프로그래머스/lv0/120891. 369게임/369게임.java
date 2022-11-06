2207b4db52384bdfb8969816ec2e0531150531aa
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-11-06 15:41:26 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        String s = String.valueOf(order);
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '3' || s.charAt(i) == '6' || s.charAt(i) == '9') {
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+}
