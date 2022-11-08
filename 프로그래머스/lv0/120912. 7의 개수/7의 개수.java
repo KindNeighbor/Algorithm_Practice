3e83d2e9e0a142c94a2176b70dfa03d6c4e5899b
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-11-08 15:09:07 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        
+        int cnt = 0;
+        for (int i = 0; i < array.length; i++) {
+            while (array[i] > 0) {
+                if (array[i] % 10 == 7) {
+                    cnt++;
+                }
+                
+                array[i] /= 10;
+            }
+        }
+        return cnt;
+    }
+}
