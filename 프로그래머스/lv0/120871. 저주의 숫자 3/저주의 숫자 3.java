e8ee09fb33bbcac04f1041b3331221b3a06009ec
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2022-11-11 20:51:25 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -2,36 +2,15 @@ class Solution {
     
     public int solution(int n) {
         
-        int cnt = 0;
-        
-        for (int i = 0; i < 10000; i++) {
-            if (curseNumber(i)) {
-                cnt++;
-            }
-            
-            if (cnt == n) {
-                return i;
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            answer++;
+            if (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                i--;
             }
         }
-        return 0;
-    }
-    
-    public boolean curseNumber(int n) {
-        
-        if (n % 3 == 0) {
-            return false;
-        }
-        
-        
-        while (n > 1) {
-            int num = n % 10;
-            if (num == 3) {
-                return false;
-            }
-            
-            n /= 10;
-        }
-        
-        return true;
+
+        return answer;
     }
 }
