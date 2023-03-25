e18b5f51bd52bdbaf04a45c76b011e8a4f6c15bf
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-03-25 13:59:06 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 480 ms, Memory: 112024 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[][] arr;
-    static int N,M;
-
-    static int count;
-    static boolean[] visited;
-    public static void dfs(int x) {
-        visited[x] = true;
-        for (int i = 0; i < N + 1; i++) {
-            if(arr[x][i] == 1 && visited[i] == false) {
-                dfs(i);
-            }
-        }
-    }
-
+    
+    static int[] parents;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+1][N+1];
-        visited = new boolean[N+1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parents = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            parents[i] = i;
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            union(u,v);
         }
 
-        for (int i = 1; i <= N; i++) {
-            if(!visited[i]) {
-                dfs(i);
-                count++;
-            }
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 1; i < N + 1; i++) {
+            hashSet.add(find(i));
         }
+        
+        System.out.println(hashSet.size());
+    }
 
-        System.out.println(count);
+    public static void union(int a, int b) {
+        int aP = find(a);
+        int bP = find(b);
+
+        if (aP != bP) {
+            parents[aP] = bP;
+        }
+    }
+
+    public static int find(int a) {
+        if (a == parents[a]) {
+            return a;
+        }
+        return parents[a] = find(parents[a]);
     }
 }
