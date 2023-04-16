284f15b1ed95fb1c9735091cd518aec8f58f5f77
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-04-16 19:57:17 +0900

    [Silver IV] Title: 로프, Time: 416 ms, Memory: 27192 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,26 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        
+        int N = Integer.parseInt(br.readLine());
+        
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < T; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+        
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(br.readLine());
+            list.add(a);
         }
-
-        ArrayList<Integer> list2 = new ArrayList<>();
+        
         Collections.sort(list);
+        
+        int max = 0;
+        
         for (int i = 0; i < list.size(); i++) {
-            list2.add(list.get(i)*(list.size()-i));
+            int sum = list.get(i) * (list.size() - i);
+            if (sum >= max) {
+                max = sum;
+            }
         }
-        Collections.sort(list2);
-        System.out.println(list2.get(list2.size() - 1));
-    }
+        
+        System.out.println(max);
+    } 
 }
