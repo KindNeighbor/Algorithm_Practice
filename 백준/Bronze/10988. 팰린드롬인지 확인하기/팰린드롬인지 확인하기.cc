6691a57622f57fc2516c3981f216eb2fc7eb4c7e
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-04-27 01:40:52 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    string str;
+    cin >> str;
+ 
+    int len = str.length();
+ 
+    int cnt = 0;
+    
+    for(int i = 0; i< len / 2; i++){
+        if(str[i] != str[str.length() - i - 1]){
+            cout << "0";
+            return 0;
+        }
+    }
+    cout << "1";
+}
