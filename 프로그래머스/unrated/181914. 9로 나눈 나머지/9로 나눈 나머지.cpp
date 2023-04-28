9d8d01fc072e25a0c4362fcecd78d89e563ae630
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-04-28 23:02:25 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int solution(string number) {
     int sum = 0;
     for (int i = 0; i < number.size(); i++) {
-        sum += number[i] - 48;
+        sum += number[i] - '0';
     }
     
     return sum % 9;
