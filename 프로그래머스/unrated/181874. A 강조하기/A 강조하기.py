472e06b871b9d6f40b326e821324cc89b20dd644
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-05-04 02:00:47 +0900

    [unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    a = ""
-    myString = myString.lower()
-    for s in myString:
-        if s == 'a':
-            s = 'A'
-            a += s
-        else:
-            a += s
-    return a
+    return myString.lower().replace('a','A')
