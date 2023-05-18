c8edde4205d90b1ecaf56ae33b6e0d8169f5421a
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-05-18 14:09:58 +0900

    [unrated] Title: 둘만의 암호, Time: 8.89 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        int[] alpha = new int[26];
+        for (int i = 0; i < skip.length(); i++) {
+            alpha[skip.charAt(i) - 'a'] = 1;
+        }
+
+        String ss = "";
+        for (int i = 0; i < s.length(); i++) {
+            int a = s.charAt(i) - 'a';
+            for (int j = 0; j < index; j++) {
+                if (a == 25) {
+                    a = 0;
+                } else {
+                    a++;
+                }
+                if (alpha[a] == 1) {
+                    j--;
+                    continue;
+                }
+            }
+            ss += (char)(a + 'a');
+        }
+        return ss;
+    }
+}
