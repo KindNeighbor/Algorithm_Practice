c8fc4946f50c836b97140033a9886e2ab5105655
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-07-30 23:24:00 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 308 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 2*N-1; j > 2*i; j--) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
