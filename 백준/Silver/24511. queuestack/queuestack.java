0108fb80e16b6cae9fae1d6ed04c5c173f32edc1
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-08-22 02:20:17 +0900

    [Silver III] Title: queuestack, Time: 632 ms, Memory: 62560 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] type = new int[N];
+        Deque<Integer> dq = new LinkedList<>();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            type[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (type[i] == 0) {
+                dq.addLast(a);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int c = Integer.parseInt(st.nextToken());
+            dq.addFirst(c);
+            sb.append(dq.removeLast()).append(" ");
+        }
+        
+        System.out.println(sb);
+    }
+}
