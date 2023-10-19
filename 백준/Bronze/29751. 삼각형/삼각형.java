76827a4cf2461369e77ca491e58ce2b4db5720d9
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2023-10-20 03:45:18 +0900

    [Bronze V] Title: 삼각형, Time: 132 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        double ans = W * H * 0.5;
+        System.out.printf("%.1f", ans);
+    }
+}
