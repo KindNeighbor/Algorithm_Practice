9d8ad7fa2d9ba69311c26bb12c77995057080153
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-01-06 12:57:37 +0900

    [Silver IV] Title: DNA, Time: 132 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.java
+++ 백준/Silver/1969. DNA/DNA.java
@@ -8,73 +8,46 @@ public class Main {
         
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        List<String> sList = new ArrayList<>();
+        
+        char[][] arr = new char[N][M];
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            sList.add(s);
+            arr[i] = br.readLine().toCharArray();
         }
+        
         StringBuilder sb = new StringBuilder();
         int sum = 0;
         for (int i = 0; i < M; i++) {
-            int A = 0; int C = 0; int G = 0; int T = 0; 
+            int[] counts = new int[4]; // A, C, G, T
+            
             for (int j = 0; j < N; j++) {
-                if (sList.get(j).charAt(i) == 'A') {
-                    A += 1;
-                } else if (sList.get(j).charAt(i) == 'C') {
-                    C += 1;
-                } else if (sList.get(j).charAt(i) == 'G') {
-                    G += 1;
-                } else if (sList.get(j).charAt(i) == 'T') {
-                    T += 1;
+                char nucleotide = arr[j][i];
+                if (nucleotide == 'A') {
+                    counts[0]++;
+                } else if (nucleotide == 'C') {
+                    counts[1]++;
+                } else if (nucleotide == 'G') {
+                    counts[2]++;
+                } else if (nucleotide == 'T') {
+                    counts[3]++;
                 }
             }
-            if (A > C && A > G && A > T) {
-                sb.append('A');
-                sum += N - A;
-            } else if (C > A && C > G && C > T) {
-                sb.append('C');
-                sum += N - C;
-            } else if (G > A && G > C && G > T) {
-                sb.append('G');
-                sum += N - G;
-            } else if (T > A && T > C && T > G) {
-                sb.append('T');
-                sum += N - T;
-            } else if (A == C && A > G && A > T) {
-                sb.append('A');
-                sum += N - A;
-            } else if (A == G && A > C && A > T) {
-                sb.append('A');
-                sum += N - A;
-            } else if (A == T && A > C && A > G) {
-                sb.append('A');
-                sum += N - A;
-            } else if (C == G && C > A && C > T) {
-                sb.append('C');
-                sum += N - C;
-            } else if (C == T && C > A && C > G) {
-                sb.append('C');
-                sum += N - C;
-            } else if (G == T && G > A && G > C) {
-                sb.append('G');
-                sum += N - G;
-            } else if (A == C && C == G && A > T) {
-                sb.append('A');
-                sum += N - A;
-            } else if (A == C && C == T && A > G) {
-                sb.append('A');
-                sum += N - A;
-            } else if (A == G && G == T && A > C) {
-                sb.append('A');
-                sum += N - A;
-            } else if (C == G && G == T && C > A) {
-                sb.append('C');
-                sum += N - C;
-            } else if (A == C && C == G && G == T) {
-                sb.append('A');
-                sum += N - A;
+            
+            int maxCount = 0;
+            char mostFrequent = ' ';
+            for (int k = 0; k < 4; k++) {
+                if (counts[k] > maxCount) {
+                    maxCount = counts[k];
+                    if (k == 0) mostFrequent = 'A';
+                    else if (k == 1) mostFrequent = 'C';
+                    else if (k == 2) mostFrequent = 'G';
+                    else if (k == 3) mostFrequent = 'T';
+                }
             }
+            
+            sb.append(mostFrequent);
+            sum += N - maxCount;
         }
+        
         System.out.println(sb);
         System.out.println(sum);
     }
