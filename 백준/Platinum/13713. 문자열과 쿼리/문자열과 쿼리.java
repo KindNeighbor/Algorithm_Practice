52991e54b5ae558d4aa3525f9ef2bf2790e43f73
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-02-09 16:08:04 +0900

    [Platinum V] Title: 문자열과 쿼리, Time: 1464 ms, Memory: 44708 KB -BaekjoonHub

--- 백준/Platinum/13713. 문자열과 쿼리/문자열과 쿼리.java
+++ 백준/Platinum/13713. 문자열과 쿼리/문자열과 쿼리.java
@@ -4,28 +4,44 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        StringBuilder sb = new StringBuilder(s).reverse();
-        ArrayList<Integer> Z = ZFunction(sb.toString());
-        int q = Integer.parseInt(br.readLine());
-        for (int i = 0; i < q; i++) {
-            int t = Integer.parseInt(br.readLine());
-            System.out.println(Z.get(sb.length() - t));
+        String S = br.readLine();
+        String reversedS = new StringBuilder(S).reverse().toString();
+        int[] Z = calculateZ(reversedS);
+
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int query = Integer.parseInt(br.readLine());
+            System.out.println(Z[Z.length - query]);
         }
     }
+    
+    static int[] calculateZ(String input) {
+        int len = input.length();
+        int[] Z = new int[len];
+        Z[0] = len;
 
-    static ArrayList<Integer> ZFunction(String s) {
-        int n = s.length(), l = -1, r = -1;
-        ArrayList<Integer> Z = new ArrayList<>(Collections.nCopies(n, 0));
-        Z.set(0, n);
-        for (int i = 1; i < n; i++) {
-            if (i <= r) Z.set(i, Math.min(r - i + 1, Z.get(i - l)));
-            while (i + Z.get(i) < n && s.charAt(Z.get(i)) == s.charAt(i + Z.get(i))) {
-                Z.set(i, Z.get(i) + 1);
-            }
-            if (r < i + Z.get(i) - 1) {
-                l = i;
-                r = i + Z.get(i) - 1;
+        int left = 0;
+        int right = 0;
+        for (int k = 1; k < len; k++) {
+            if (k > right) {
+                left = right = k;
+                while (right < len && input.charAt(right - left) == input.charAt(right)) {
+                    right++;
+                }
+                Z[k] = right - left;
+                right--;
+            } else {
+                int k1 = k - left;
+                if (Z[k1] < right - k + 1) {
+                    Z[k] = Z[k1];
+                } else {
+                    left = k;
+                    while (right < len && input.charAt(right - left) == input.charAt(right)) {
+                        right++;
+                    }
+                    Z[k] = right - left;
+                    right--;
+                }
             }
         }
         return Z;
