e8ddad99da9e255f8ab9b0dd26092d2520c740c7
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-02-09 15:05:42 +0900

    [Platinum IV] Title: 광고, Time: 288 ms, Memory: 23972 KB -BaekjoonHub

---
+++ 백준/Platinum/1305. 광고/광고.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        
+        int[] table = new int[L];
+        int j = 0;
+        
+        for (int i = 1; i < L; i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = table[j - 1];
+            }
+            
+            if (s.charAt(i) == s.charAt(j)) {
+                table[i] = ++j;
+            }
+        }
+        
+        System.out.println(L - table[L - 1]);
+    }
+}
