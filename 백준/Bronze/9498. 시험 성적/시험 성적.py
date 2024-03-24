ff0fff63ded4d0174156d5207fe61b7a376397a6
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-03-24 18:05:31 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+A = int(input())
+
+if A >= 90:
+    print('A')
+elif A < 90 and A >= 80:
+    print('B')
+elif A < 80 and A >= 70:
+    print('C')
+elif A < 70 and A >= 60:
+    print('D')
+elif A < 60:
+    print('F')
+
