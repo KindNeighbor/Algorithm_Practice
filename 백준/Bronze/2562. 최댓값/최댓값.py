36f8c214b8346ef34244c30effa275bf43f5df05
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-04-03 22:28:39 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,12 +1,12 @@
 numbers = [int(input()) for _ in range(9)]
 
-max_value = numbers[0]
-max_index = 1
+maxValue = numbers[0]
+maxIdx = 1
 
 for i in range(1, 9):
-    if numbers[i] > max_value:
-        max_value = numbers[i]
-        max_index = i + 1
+    if numbers[i] > maxValue:
+        maxValue = numbers[i]
+        maxIdx = i + 1
 
-print(max_value)
-print(max_index)
+print(maxValue)
+print(maxIdx)
