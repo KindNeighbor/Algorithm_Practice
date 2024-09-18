6a8646092ea371e5df64c785374e92288a3b28ac
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-09-18 20:25:42 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -14,7 +14,7 @@ public class Main {
 
         if (a == 60 && b == 60 && c == 60) {
             System.out.println("Equilateral");
-        } else if ((a + b + c == 180) && a == b || b == c || c == a) {
+        } else if ((a + b + c == 180) && (a == b || b == c || c == a)) {
             System.out.println("Isosceles");
         } else if ((a + b + c == 180) && a != b && b != c && c != a) {
             System.out.println("Scalene");
