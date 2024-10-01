485cb37b7a9e6841e94a9f38194913d0591793a2
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-10-01 18:35:04 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,20 @@
+dial = {
+    'A': 2, 'B': 2, 'C': 2,
+    'D': 3, 'E': 3, 'F': 3,
+    'G': 4, 'H': 4, 'I': 4,
+    'J': 5, 'K': 5, 'L': 5,
+    'M': 6, 'N': 6, 'O': 6,
+    'P': 7, 'Q': 7, 'R': 7, 'S': 7,
+    'T': 8, 'U': 8, 'V': 8,
+    'W': 9, 'X': 9, 'Y': 9, 'Z': 9
+}
+
+def cal(word):
+    time = 0
+    for s in word:
+        time += dial[s] + 1
+    return time
+
+word = input()
+
+print(cal(word))
