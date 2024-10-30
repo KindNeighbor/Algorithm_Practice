0fa7bcd4f3a6c85b8be122296bd0f795f4065281
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-10-30 22:09:37 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,38 +1,19 @@
+def get_fraction_position(X):
+    current_sum = 1
+    level = 2
+    is_top_direction = False
+    
+    while current_sum < X:
+        current_sum += level
+        level += 1
+        is_top_direction = not is_top_direction
+        
+    position = current_sum - X
+    position += 1
+    
+    if is_top_direction:
+        return f"{level - position}/{position}"
+    return f"{position}/{level - position}"
+
 X = int(input())
-
-# 2 / 3 3 / 4 4 4 / 5 5 5 5 
-# 1/1 | 1/2 2/1 | 3/1 2/2 1/3 | 1/4 2/3 3/2 4/1 |
-# 1 / 1 2 / 3 2 1 / 1 2 3 4 / 5 4 3 2 1
-# 1 / 2 1 / 1 2 3 / 4 3 2 1 / 1 2 3 4 5
-
-top = 0
-bot = 1
-idx = 2
-num = 1
-
-son = -1
-mom = -1
-while(True):
-    if num < X:
-        num += idx
-        idx += 1
-        if top == 0:
-            top = 1
-        else:
-            top = 0
-        if bot == 0:
-            bot = 1
-        else:
-            bot = 0
-    else:
-        a = num - X
-        a += 1
-        if top == 1:
-            son = idx - a
-            mom = a
-        else :
-            son = a
-            mom = idx - a
-        break
-            
-print(str(son)+"/"+str(mom))
+print(get_fraction_position(X))
