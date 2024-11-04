459ba4aea22b175e0e48f9e75bb8ea6919610ce6
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-04 23:26:18 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,9 +1,13 @@
 N = int(input())
 
-idx = 2
-while(N > 1):
-    if N % idx == 0:
-        N /= idx
-        print(idx)
-    else:
-        idx += 1
+while N % 2 == 0:
+    print(2)
+    N //= 2
+
+for i in range(3, int(N ** 0.5) + 1, 2):
+    while N % i == 0:
+        print(i)
+        N //= i
+
+if N > 1:
+    print(N)
