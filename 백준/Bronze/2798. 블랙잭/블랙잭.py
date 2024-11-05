e2593b09272f8b0d2bd423ac985ebed96f3ea6a8
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-05 22:29:24 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,17 +1,15 @@
-N,M = map(int, input().split())
+N, M = map(int, input().split())
 
-A = list(map(int, input().split()))
+list1 = list(map(int, input().split()))
 
-near = 0
+maxSum = -1
 
-for i in range(0, len(A)):
-    for j in range(i + 1, len(A)):
-        for k in range(j + 1, len(A)):
-            sum = A[i] + A[j] + A[k]
-            if near < sum and sum <= M:
-                near = sum
-                
-print(near)
-            
-    
+for i in range(0, N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            sum1 = list1[i] + list1[j] + list1[k]
+            if sum1 <= M:
+                if sum1 >= maxSum:
+                    maxSum = sum1
 
+print(maxSum)
