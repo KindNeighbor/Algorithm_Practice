fd4cb3d410acfb77dec08c1fc6ed19a5efda07fe
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-09 19:57:59 +0900

    [Silver I] Title: 회의실 배정, Time: 232 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,20 @@
 import sys
+
 N = int(sys.stdin.readline())
 meetings = []
 
 for _ in range(N):
-    start, end = map(int, sys.stdin.readline().split())
-    meetings.append((start, end))
+    a, b = map(int, sys.stdin.readline().split())
+    meetings.append((a, b))
 
 meetings.sort(key=lambda x: (x[1], x[0]))
 
 cnt = 1
-end_time = meetings[0][1]
+endT = meetings[0][1]
 
 for i in range(1, N):
-    if meetings[i][0] >= end_time:
+    if meetings[i][0] >= endT:
+        endT = meetings[i][1]
         cnt += 1
-        end_time = meetings[i][1]
-
+        
 print(cnt)
