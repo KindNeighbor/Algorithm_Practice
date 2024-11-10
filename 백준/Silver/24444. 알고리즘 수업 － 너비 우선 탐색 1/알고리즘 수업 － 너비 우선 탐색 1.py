e181f8d47991c0b279795019c65074f24032689b
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-10 23:04:12 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 512 ms, Memory: 59772 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+
+def bfs(graph, R, N):
+    visited = [False] * (N + 1)
+    order = [0] * (N + 1)
+    queue = deque([R])
+    visited[R] = True
+    cnt = 1
+    order[R] = cnt
+    
+    while queue:
+        current = queue.popleft()
+        
+        for next_node in sorted(graph[current]):
+            if not visited[next_node]:
+                cnt += 1
+                order[next_node] = cnt
+                visited[next_node] = True
+                queue.append(next_node)
+                
+    return order
+
+N, M, R = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+result = bfs(graph, R, N)
+
+for i in range(1, N + 1):
+    print(result[i])
