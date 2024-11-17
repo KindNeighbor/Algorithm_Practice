24d23f894d4ee20a49e39830a382964d0f38ec8f
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-17 23:32:21 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,6 +2,9 @@ import sys
 from collections import deque
 sys.setrecursionlimit(10**6)
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 def bfs(x, y):
     visited[x][y] = 1
     queue = deque([(x, y)])
@@ -13,22 +16,20 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == '1' and visited[nx][ny] == 0:
-                queue.append((nx, ny))
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and graph[nx][ny] == 1:
                 visited[nx][ny] = visited[x][y] + 1
-                
-    return visited[N - 1][M - 1]
+                queue.append((nx, ny))
+               
+    return visited
 
 N, M = map(int, sys.stdin.readline().split())
 
 graph = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
 for _ in range(N):
-    graph.append(list(sys.stdin.readline().strip()))
+    graph.append(list(map(int, sys.stdin.readline().strip())))
     
 visited = [[0] * M for _ in range(N)]
-
-print(bfs(0, 0))
-    
+result = []
+result = bfs(0, 0)
+print(result[N - 1][M - 1])
