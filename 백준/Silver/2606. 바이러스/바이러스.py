96d5e9b6de924090933922bb411dbae1d9fea23e
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-17 22:34:58 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,10 +2,12 @@ import sys
 sys.setrecursionlimit(10**6)
 
 def dfs(graph, R, visited):
+    global cnt
     visited[R] = True
+    cnt += 1
     
-    for next_node in sorted(graph[R]):
-        if not visited[next_node]:
+    for next_node in graph[R]:
+        if visited[next_node] is False:
             dfs(graph, next_node, visited)
 
 N = int(sys.stdin.readline())
@@ -17,8 +19,10 @@ for _ in range(M):
     u, v = map(int, sys.stdin.readline().split())
     graph[u].append(v)
     graph[v].append(u)
-    
+
 visited = [False] * (N + 1)
+cnt = 0
 
 dfs(graph, 1, visited)
-print(sum(visited) - 1)
+
+print(cnt - 1)
