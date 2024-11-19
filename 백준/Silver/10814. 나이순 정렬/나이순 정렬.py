8435a5039bed3b4cc8abde4fc632d06b073ce404
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-19 23:58:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 216 ms, Memory: 46140 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+arr = []
+
+for _ in range(N):
+    age, name = sys.stdin.readline().split()
+    age = int(age)
+    arr.append((age, name))
+    
+arr.sort(key=lambda x : (x[0]))
+
+for age, name in arr:
+    print(age, name)
