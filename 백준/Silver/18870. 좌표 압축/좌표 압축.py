d4cada81edbd62c946cd33c493fba5045704c384
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2024-11-20 00:17:12 +0900

    [Silver II] Title: 좌표 압축, Time: 1500 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -2,11 +2,6 @@ import sys
 N = int(sys.stdin.readline())
 
 arr = list(map(int, sys.stdin.readline().split()))
-
-set1 = set(arr)
-
-sorted_list = sorted(set1)
-
 compressed = {value: idx for idx, value in enumerate(sorted(set(arr)))}
 
 result = [compressed[x] for x in arr]
