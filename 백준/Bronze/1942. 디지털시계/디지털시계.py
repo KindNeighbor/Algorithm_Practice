e8d66f36b64c9f33352b28bc63fe9d37a1624da2
YoungJoon_Choi <97837003+KindNeighbor@users.noreply.github.com>
2025-01-11 23:33:09 +0900

    [Bronze I] Title: 디지털시계, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1942. 디지털시계/디지털시계.py
+++ 백준/Bronze/1942. 디지털시계/디지털시계.py
@@ -1,59 +1,37 @@
 import sys
 
 def change(time):
-   h, m, s = map(int, time.split(":"))
-   return h * 10000 + m * 100 + s
+    h, m, s = map(int, time.split(":"))
+    return h * 10000 + m * 100 + s
 
 def is_valid_time(t):
-   h = t // 10000
-   m = (t % 10000) // 100
-   s = t % 100
-   return 0 <= h <= 23 and 0 <= m <= 59 and 0 <= s <= 59
+    h = t // 10000
+    m = (t % 10000) // 100
+    s = t % 100
+    return 0 <= h <= 23 and 0 <= m <= 59 and 0 <= s <= 59
 
-def count_3_multiples_in_segment(start, end):
-   if start > end:
-       return 0
-   if start < 6000 and end > 9999:
-       return (count_3_multiples_in_segment(start, 5959) + 
-               count_3_multiples_in_segment(10000, end))
-   elif 6000 <= start <= 9999 or 6000 <= end <= 9999:
-       return 0
-       
-   first = start + (3 - (start % 3)) % 3
-   while first <= end and not is_valid_time(first):
-       first += 3
-       
-   last = end - (end % 3)
-   while last >= start and not is_valid_time(last):
-       last -= 3
-       
-   if first > end or last < start:
-       return 0
-   
-   count = 0
-   for num in range(first, last + 1, 3):
-       if is_valid_time(num):
-           count += 1
-   return count
+def count_multiples(start, end):
+    if start < 6000 and end > 9999:
+        return count_multiples(start, 5959) + count_multiples(10000, end)
+    if 6000 <= start <= 9999 or 6000 <= end <= 9999:
+        return 0
 
-def count_in_range(start, end):
-   if end < start:
-       return (count_3_multiples_in_segment(start, 235959) + 
-               count_3_multiples_in_segment(0, end))
-   else:
-       return count_3_multiples_in_segment(start, end)
+    curr = start + (3 - (start % 3)) % 3
+    end = end - (end % 3)
+    
+    count = 0
+    while curr <= end:
+        if is_valid_time(curr):
+            count += 1
+        curr += 3
+    return count
 
-a1, b1 = sys.stdin.readline().split()
-a2, b2 = sys.stdin.readline().split()
-a3, b3 = sys.stdin.readline().split()
-
-int_a1 = change(a1)
-int_b1 = change(b1)
-int_a2 = change(a2)
-int_b2 = change(b2)
-int_a3 = change(a3)
-int_b3 = change(b3)
-
-print(count_in_range(int_a1, int_b1))
-print(count_in_range(int_a2, int_b2))
-print(count_in_range(int_a3, int_b3))
+times = [sys.stdin.readline().split() for _ in range(3)]
+for start, end in times:
+    start_time = change(start)
+    end_time = change(end)
+    
+    if end_time < start_time:
+        print(count_multiples(start_time, 235959) + count_multiples(0, end_time))
+    else:
+        print(count_multiples(start_time, end_time))
